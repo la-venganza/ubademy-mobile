b6275b1a51632641a3c80c84b6b69b6acd248f44
luciana <lpiazzi@despegar.com>
2021-10-29 11:14:00 -0300

    fixes requested

--- src/actions/googleAuthentication.js
+++ src/actions/googleAuthentication.js
@@ -1,18 +1,5 @@
 import * as Google from 'expo-google-app-auth';
-import { initializeApp } from 'firebase/app';
 import Constants from 'expo-constants';
-
-const firebaseConfig = {
-  apiKey: Constants.manifest.extra.FIREBASE_API_KEY,
-  authDomain: Constants.manifest.extra.FIREBASE_AUTH_DOMAIN,
-  projectId: Constants.manifest.extra.FIREBASE_PROJECT_ID,
-  storageBucket: Constants.manifest.extra.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: Constants.manifest.extra.FIREBASE_MESSAGING_SENDER_ID,
-  appId: Constants.manifest.extra.FIREBASE_APP_ID,
-};
-
-// Initialize Firebase
-initializeApp(firebaseConfig);
 
 // eslint-disable-next-line import/prefer-default-export
 export async function signInWithGoogleAsync() {
