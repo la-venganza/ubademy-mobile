aaa411346dcba9582c9aad34d6fda9f857334d41
recheconea <recheconea@sparkdigital.com>
2021-12-09 16:40:27 -0300

    corrected subscription endpoints

--- src/services/subscriptionService.ts
+++ src/services/subscriptionService.ts
@@ -10,9 +10,12 @@ const getSubscription = async (userId: string) => {
   }
 };
 
-const updateSubscription = async (userId, newPlan) => {
+const updateSubscription = async (userId:string, newPlan:string) => {
   try {
-    const response = await instance.put(`subscription/${userId}`, { plan: newPlan });
+    const now = new Date();
+    const endDate = new Date(now.setMonth(now.getMonth() + 1));
+    const endDateFormatted = `${endDate.getFullYear()}-${endDate.getMonth() + 1}-${endDate.getDate()}`;
+    const response = await instance.post(`subscription/${userId}`, { subscription: newPlan.toLowerCase(), end_date: endDateFormatted });
     return response.data;
   } catch (error) {
     console.log(error);
--- src/views/planSelection.tsx
+++ src/views/planSelection.tsx
@@ -77,8 +77,8 @@ const PlanSelection = ({ navigation }) => {
       const balance = await walletService.getBalance(authContext.userId);
       setAvailableMoney(balance?.balance ?? 0);
       const subscription = await subscriptionService.getSubscription(authContext.userId);
-      setCurrentPlan(subscription?.subscription?.title ?? 'Free');
-      setSelectedPlan(subscription?.subscription?.title ?? 'Free');
+      setCurrentPlan(subscription?.results[0].subscription.title ?? 'Free');
+      setSelectedPlan(subscription?.results[0].subscription.title ?? 'Free');
     };
     getBalance();
   }, []);
