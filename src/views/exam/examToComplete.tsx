922de38a22cf048b433b622067de282d00de8c4e
luciana <lpiazzi@despegar.com>
2021-12-14 12:40:29 -0300

    exam corrections

--- src/views/exam/examToComplete.tsx
+++ src/views/exam/examToComplete.tsx
@@ -11,6 +11,7 @@ import IExam from '../../interfaces/IExam';
 import ExamMultipleChoice from '../../components/ExamMultipleChoice';
 import ExamDevelopQuestion from '../../components/ExamDevelopQuestion';
 import IExamAnswer from '../../interfaces/IExamAnswer';
+import ExamView from '../../components/ExamView';
 
 const styles = StyleSheet.create({
   container: {
@@ -36,86 +37,19 @@ interface Props {
 const ExamToCompleteScreen = ({
   navigation, route,
 } : Props) => {
-  const [exam, setExam] = useState<IExam>({});
-
   const {
     courseId, lessonId, examId, userId,
   } = route.params;
 
-  const [answers, setAnswers] = useState({});
-
-  const setAnswer = (answer: IExamAnswer) => {
-    const qId = answer.questionId;
-    const newAnswers = {
-      ...answers,
-      [qId]: answer,
-    };
-    setAnswers(newAnswers);
-  };
-
-  const isFocused = useIsFocused();
-
-  const fetchExam = async () => {
-    const examData = await examService.getExam(courseId, lessonId, userId, examId);
-    if (examData) {
-      setExam(examData);
-    }
-  };
-
-  const submit = () => {
-    examService.submitExamAnswers(examId, courseId, lessonId, userId, Object.values(answers));
-    navigation.goBack(null);
-  };
-
-  const cancelAll = () => {
-    navigation.goBack(null);
-  };
-
-  useEffect(() => {
-    fetchExam();
-  }, [isFocused]);
-
-  function renderMultipleChoice(choice) {
-    return (<ExamMultipleChoice examMultipleChoice={choice} setAnswer={setAnswer} />);
-  }
-
-  function renderDevelopQuestion(developQuestion) {
-    return (
-      <ExamDevelopQuestion
-        examDevelopQuestion={developQuestion}
-        returnAnwser={setAnswer}
-      />
-    );
-  }
-
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        {exam.questions
-          ? (
-            <>
-              <ScrollView>
-                <View>
-                  <Card.Title
-                    title={exam.title}
-                    subtitle="Exam stage"
-                  />
-                  {exam.questions.map((question) => {
-                    if (question.type === 'develop') {
-                      return renderDevelopQuestion(question.developQuestion);
-                    }
-                    return renderMultipleChoice(question.multipleChoiceQuestion);
-                  })}
-                </View>
-                <View style={styles.menuWrapper}>
-                  <Button onPress={cancelAll}>Cancel</Button>
-                  <Button mode="contained" labelStyle={{ color: 'white' }} onPress={submit}>Submit</Button>
-                </View>
-              </ScrollView>
-            </>
-          ) : <ActivityIndicator size="large" color="#00ff00" /> }
-      </View>
-    </SafeAreaView>
+    <ExamView
+      navigation={navigation}
+      courseId={courseId}
+      lessonId={lessonId}
+      examId={examId}
+      userId={userId}
+      readOnly={false}
+    />
   );
 };
 
